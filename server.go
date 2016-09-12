a7f3a5a39ffa28fe4ad56e4a7c48e2eb38a99577
Arthelon <hsing.daniel@gmail.com>
2016-09-12 13:01:05 +0800

    Created RPC methods

--- server.go
+++ server.go
@@ -4,28 +4,67 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
 	"net"
-	"context"
+	"golang.org/x/net/context"
 	pb "github.com/Arthelon/grpc-fake/proto"
+	"github.com/icrowley/fake"
 )
 
-type fakeGeneratorServer struct {
+type fakeGeneratorServer struct {}
 
+func generateAddress() pb.Address {
+	return pb.Address{
+		Continent: fake.Continent(),
+		Country: fake.Country(),
+		City: fake.City(),
+		State: fake.State(),
+		StreetAddress: fake.StreetAddress(),
+		Zip: fake.Zip(),
+		Latitude: fake.Latitute(),
+		Longitude: fake.Longitude(),
+	}
 }
 
-func (fg *fakeGeneratorServer) GetUser(ctx context.Context, _ *pb.EmptyMessage) {
-
+func generateDate() pb.Date {
+	return pb.Date{
+		Year: int64(fake.Year(1000, 2016)),
+		Month: int64(fake.MonthNum()),
+		Day: int64(fake.Day()),
+	}
 }
 
-func (fg *fakeGeneratorServer) GetAddress(ctx context.Context, _ *pb.EmptyMessage) {
-
+func (fg *fakeGeneratorServer) GetUser(ctx context.Context, in *pb.EmptyMessage) (*pb.User, error) {
+	date := generateDate()
+	address := generateAddress()
+	return &pb.User{
+		FullName: fake.FullName(),
+		Username: fake.UserName(),
+		Password: fake.SimplePassword(),
+		Email: fake.EmailAddress(),
+		PhoneNumber: fake.Phone(),
+		Gender: fake.Gender(),
+		Language: fake.Language(),
+		Birthday: &date,
+		Address: &address,
+	}, nil
 }
 
-func (fg *fakeGeneratorServer) GetEmail(ctx context.Context, _ *pb.EmptyMessage) {
-
+func (fg *fakeGeneratorServer) GetAddress(ctx context.Context, in *pb.EmptyMessage) (*pb.Address, error) {
+	address := generateAddress()
+	return &address, nil
 }
 
-func (fg *fakeGeneratorServer) GetDate(ctx context.Context, _ *pb.EmptyMessage) {
+func (fg *fakeGeneratorServer) GetEmail(ctx context.Context, in *pb.EmptyMessage) (*pb.Email, error) {
+	return &pb.Email{
+		Title: fake.EmailSubject(),
+		Body: fake.EmailBody(),
+		From: fake.EmailAddress(),
+		To: fake.EmailAddress(),
+	}, nil
+}
 
+func (fg *fakeGeneratorServer) GetDate(ctx context.Context, in *pb.EmptyMessage) (*pb.Date, error) {
+	date := generateDate()
+	return &date, nil
 }
 
 
@@ -36,6 +75,6 @@ func main() {
 		return
 	}
 	grpcServer := grpc.NewServer()
-	pb.RegisterFakeGeneratorServer(&grpcServer, fakeGeneratorServer{})
+	pb.RegisterFakeGeneratorServer(grpcServer, &fakeGeneratorServer{})
 	grpcServer.Serve(listener)
 }
